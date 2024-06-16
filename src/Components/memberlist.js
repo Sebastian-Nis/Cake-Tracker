9ff014a55195981e5d88aae806326002785cd97b
Sebastian-Nis <sebanistor@gmail.com>
2024-06-16 17:31:46 +0300

    GET error

--- src/Components/memberlist.js
+++ src/Components/memberlist.js
@@ -4,10 +4,11 @@ const MemberList = () => {
   const [members, setMembers] = useState([]);
   const [filteredMembers, setFilteredMembers] = useState([]);
   const [searchQuery, setSearchQuery] = useState('');
+  const [sortConfig, setSortConfig] = useState({ key: null, direction: 'ascending' });
 
   const fetchMembers = async () => {
     try {
-      const response = await fetch(`${process.env.REACT_APP_API_BASE_URL}/members`, {
+      const response = await fetch('http://localhost:3000/api/members', {
         method: 'GET',
         headers: {
           'Content-Type': 'application/json',
